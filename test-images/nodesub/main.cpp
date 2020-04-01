1e63a4c6e3f002ae682168504814639a06eb3bb6
aler9 <46489434+aler9@users.noreply.github.com>
2020-04-01 11:35:15 +0200

    cleanup test-images

--- test-images/nodesub/main.cpp
+++ test-images/nodesub/main.cpp
@@ -1,13 +1,11 @@
 
 #include <ros/ros.h>
-#include <thread>
 
 #include <nodesub/Mymsg.h>
 
 
 void on_message(const nodesub::Mymsg::ConstPtr& in) {
-    //ROS_INFO("in: %f %f %f", in->position.x, in->position.y, in->position.z);
-    //ROS_INFO("%u", in->prova);
+    ROS_INFO("in: %d", in->a);
 }
 
 int main(int argc, char **argv) {
