e84937e892dba655192a6e37ef2fd7a496d469b4
aler9 <46489434+aler9@users.noreply.github.com>
2020-06-02 22:48:28 +0200

    add node-sub-udp test image

---
+++ test-images/node-sub-udp/main.cpp
@@ -0,0 +1,20 @@
+
+#include <ros/ros.h>
+
+#include <nodesub/Mymsg.h>
+
+
+void on_message(const nodesub::Mymsg::ConstPtr& in) {
+    printf("%d %s %lu\n", in->a, in->b[0].a.c_str(), in->b[0].b);
+    //ros::shutdown();
+}
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "nodesub");
+    ros::NodeHandle node("~");
+
+    auto sub = node.subscribe("/test_pub", 100, on_message, ros::TransportHints().udp());
+
+    ros::spin();
+    return 0;
+}
