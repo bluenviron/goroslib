06a22ca07801acd9f14c532f7fc411b53d9348f2
Morgan Larsson <mlveggo@hotmail.com>
2022-08-29 14:38:59 +0200

    fix: update linter to 1.49.0 and adjust for Go 1.19 (#115)

--- node.go
+++ node.go
@@ -4,41 +4,40 @@ for the Robot Operating System (ROS).
 
 Basic example (more are available at https://github.com/aler9/goroslib/tree/master/examples):
 
-  package main
+	package main
 
-  import (
-      "fmt"
-      "github.com/aler9/goroslib"
-      "github.com/aler9/goroslib/pkg/msgs/sensor_msgs"
-  )
+	import (
+	    "fmt"
+	    "github.com/aler9/goroslib"
+	    "github.com/aler9/goroslib/pkg/msgs/sensor_msgs"
+	)
 
-  func onMessage(msg *sensor_msgs.Imu) {
-      fmt.Printf("Incoming: %+v\n", msg)
-  }
+	func onMessage(msg *sensor_msgs.Imu) {
+	    fmt.Printf("Incoming: %+v\n", msg)
+	}
 
-  func main() {
-      n, err := goroslib.NewNode(goroslib.NodeConf{
-          Name:          "goroslib",
-          MasterAddress: "127.0.0.1:11311",
-      })
-      if err != nil {
-          panic(err)
-      }
-      defer n.Close()
+	func main() {
+	    n, err := goroslib.NewNode(goroslib.NodeConf{
+	        Name:          "goroslib",
+	        MasterAddress: "127.0.0.1:11311",
+	    })
+	    if err != nil {
+	        panic(err)
+	    }
+	    defer n.Close()
 
-      sub, err := goroslib.NewSubscriber(goroslib.SubscriberConf{
-          Node:     n,
-          Topic:    "test_topic",
-          Callback: onMessage,
-      })
-      if err != nil {
-          panic(err)
-      }
-      defer sub.Close()
+	    sub, err := goroslib.NewSubscriber(goroslib.SubscriberConf{
+	        Node:     n,
+	        Topic:    "test_topic",
+	        Callback: onMessage,
+	    })
+	    if err != nil {
+	        panic(err)
+	    }
+	    defer sub.Close()
 
-      select {}
-  }
-
+	    select {}
+	}
 */
 package goroslib
 
