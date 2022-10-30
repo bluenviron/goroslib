0db38165ee30b64553e2f8e2cce167be83e7c968
aler9 <46489434+aler9@users.noreply.github.com>
2022-10-30 13:51:40 +0100

    upgrade rostopic-hz to noetic

--- testimages/rostopic-hz/start.sh
+++ testimages/rostopic-hz/start.sh
@@ -4,6 +4,6 @@ export ROS_MASTER_URI=http://$MASTER_IP:11311/
 export ROS_IP=$(hostname -i)
 export ROS_NAMESPACE=/myns
 
-stdbuf -oL -eL rostopic hz --window=5 test_topic &
+python3 -u /opt/ros/noetic/bin/rostopic hz --window=5 test_topic &
 
 sleep 2
