ca987b029755db75b136389ffcca42e1b159ada8
Alessandro Ros <aler9.dev@gmail.com>
2024-05-08 23:51:53 +0200

    fix tests (#205)

--- testimages/node-generic/main.cpp
+++ testimages/node-generic/main.cpp
@@ -1,7 +1,7 @@
 #include <ros/ros.h>
 
 int main(int argc, char **argv) {
-    ros::init(argc, argv, "nodegen");
+    ros::init(argc, argv, "nodegeneric");
     ros::NodeHandle node;
 
     ros::spin();
